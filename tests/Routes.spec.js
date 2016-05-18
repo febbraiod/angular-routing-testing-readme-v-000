f17f940c9757046868aa73e720a1ffa848d5f028
febbraiod <donedesign1@gmail.com>
2016-05-18 17:36:01 -0400

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -15,5 +15,44 @@ describe('Routes', function () {
 
 			expect(state.url).toEqual('/settings');
 		});
+
+		it('should get the correct Template', function () {
+			state = $state.get('settings');
+
+			expect(state.templateUrl).toEqual('views/settings.html');
+		});
 	});
+
+	describe('User', function () {
+		var state;
+
+		it('should have the correct URL', function () {
+			state = $state.get('settings.user');
+
+			expect(state.url).toEqual('/user');
+		});
+
+		it('should get the correct Template', function () {
+			state = $state.get('settings.user');
+
+			expect(state.templateUrl).toEqual('views/settings/user.html');
+		});
+	});
+
+	describe('Notifications', function () {
+		var state;
+
+		it('should have the correct URL', function () {
+			state = $state.get('settings.notifications');
+
+			expect(state.url).toEqual('/notifications');
+		});
+
+		it('should get the correct Template', function () {
+			state = $state.get('settings.notifications');
+
+			expect(state.templateUrl).toEqual('views/settings/notifications.html');
+		});
+	});
+
 });
